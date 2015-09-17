d3346a4aeaed1eb85f2d3fca143f325a9e7aec1a
James Norton <james.norton@me.com>
2015-09-17 11:09:29 -0700

    Make portfolio interactive by adding/removing classes in jQuery

--- js/app.js
+++ js/app.js
@@ -2,10 +2,37 @@ $(document).ready(function() {
   $('.subnav-link').on('click', function (){
 
   var temp = $(this).text().toLowerCase();
-  console.log(temp);
 
   if (temp === "brand & logotypes") {
-    $('.brandandlogotypes').toggleClass('hidden');
+    $('.uidesign').addClass('hidden');
+    $('.iconsdesign').addClass('hidden');
+    $('.webdesign').addClass('hidden');
+    $('.brandsandlogotypes').removeClass('hidden');
+  }
+
+  if (temp === "webdesign") {
+    $('.uidesign').addClass('hidden');
+    $('.iconsdesign').addClass('hidden');
+    $('.brandsandlogotypes').addClass('hidden');
+    $('.webdesign').removeClass('hidden');
+  }
+
+  if (temp === "ui design") {
+    $('.brandsandlogotypes').addClass('hidden');
+    $('.iconsdesign').addClass('hidden');
+    $('.webdesign').addClass('hidden');
+    $('.uidesign').removeClass('hidden');
+  }
+
+  if (temp === "icons design") {
+    $('.uidesign').addClass('hidden');
+    $('.brandsandlogotypes').addClass('hidden');
+    $('.webdesign').addClass('hidden');
+    $('.iconsdesign').removeClass('hidden');
+  }
+
+  if (temp === "all works") {
+    $('.hidden').removeClass('hidden');
   }
 
   });
