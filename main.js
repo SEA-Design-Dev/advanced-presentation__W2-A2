35c6db1762b9642041b8dcbcaf47934dcb714289
Zachary Gordon <z.gordon.h@gmail.com>
2015-09-18 02:09:38 -0700

    Add portfolio filter (WIP)

--- main.js
+++ main.js
@@ -4,4 +4,37 @@ $(".graph").click(function() {
   $( 'progress' ).toggleClass("graphShow graphHide")
 })
 
+   $('.web').click(function (e){
+      e.preventDefault();
+     if ($(".notWeb").is(":hidden")){
+       $(".notWeb").slideDown("slow");
+     } else{
+      $(".notWeb").hide('slow');
+     }
+   });
+    $('.ui').click(function (e){
+      e.preventDefault();
+     if ($(".notUi").is(":hidden")){
+       $(".notUi").slideDown("slow");
+     } else{
+      $(".notUi").hide('slow');
+     }
+   });
+     $('.icon').click(function (e){
+      e.preventDefault();
+     if ($(".notIcon").is(":hidden")){
+       $(".notIcon").slideDown("slow");
+     } else{
+      $(".notIcon").hide('slow');
+     }
+    });
+    $('.brand').click(function (e){
+      e.preventDefault();
+     if ($(".notBrand").is(":hidden")){
+       $(".notBrand").slideDown("slow");
+     } else{
+      $(".notBrand").hide('slow');
+     }
+   });
+
 });
